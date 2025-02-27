1d6f9ae96f15b3b0758bae79ca83ff7bee2f2225
st <start.art220@gmail.com>
2025-02-27 19:32:49 +0200

    Improve code

--- EventCollectorService/Services/MsmqQueueService.cs
+++ EventCollectorService/Services/MsmqQueueService.cs
@@ -4,20 +4,32 @@ using Newtonsoft.Json;
 
 namespace EventCollectorService.Services
 {
+    /// <summary>
+    /// Service for interacting with MSMQ.
+    /// </summary>
     public class MsmqQueueService : IQueueService
     {
         private readonly string _queuePath = @".\private$\eventsQueue";
         private readonly ILogger<MsmqQueueService> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsmqQueueService"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
         public MsmqQueueService(ILogger<MsmqQueueService> logger)
         {
             _logger = logger;
             if (!MessageQueue.Exists(_queuePath))
             {
                 MessageQueue.Create(_queuePath);
+                _logger.LogInformation("Message queue created at path: {QueuePath}", _queuePath);
             }
         }
 
+        /// <summary>
+        /// Sends an event to the MSMQ.
+        /// </summary>
+        /// <param name="eventData">The event data to send.</param>
         public void SendEvent(EventData eventData)
         {
             try
@@ -25,6 +37,11 @@ namespace EventCollectorService.Services
                 using MessageQueue queue = new(_queuePath);
                 var jsonData = JsonConvert.SerializeObject(eventData);
                 queue.Send(jsonData);
+                _logger.LogInformation("Event sent to MSMQ: {EventData}", jsonData);
+            }
+            catch (MessageQueueException mqex)
+            {
+                _logger.LogError(mqex, "Message Queue error sending event to MSMQ");
             }
             catch (Exception ex)
             {
@@ -32,6 +49,11 @@ namespace EventCollectorService.Services
             }
         }
 
+        /// <summary>
+        /// Retrieves recent events from the MSMQ.
+        /// </summary>
+        /// <param name="count">The number of recent events to retrieve.</param>
+        /// <returns>A list of recent events.</returns>
         public List<EventData> GetRecentEvents(int count = 10)
         {
             List<EventData> events = new();
@@ -45,6 +67,11 @@ namespace EventCollectorService.Services
                     var eventData = JsonConvert.DeserializeObject<EventData>(msg.Body.ToString());
                     if (eventData != null) events.Add(eventData);
                 }
+                _logger.LogInformation("Retrieved {Count} events from MSMQ", events.Count);
+            }
+            catch (MessageQueueException mqex)
+            {
+                _logger.LogError(mqex, "Message Queue error retrieving events from MSMQ");
             }
             catch (Exception ex)
             {
@@ -55,3 +82,4 @@ namespace EventCollectorService.Services
         }
     }
 }
+
